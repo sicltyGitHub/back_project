cd106658580d025c919dba2a2b9f4e317ac41dac
siclty <1727512879@qq.com>
2019-11-15 16:46:45 +0800

    修改部分

--- 2019.11.08/wed_middle_second_teacher/src/views/Login.vue
+++ 2019.11.08/wed_middle_second_teacher/src/views/Login.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="login">
-    <van-nav-bar
-      title="登录"
-      left-arrow
-      @click-left="$router.push('/')" />
+    <van-nav-bar title="登录" left-arrow @click-left="$router.push('/')" />
 
-      <van-cell-group>
-        <van-field
-          v-model="form.mobile"
-          required
-          clearable
-          left-icon="manager"
-          label="手机号码"
-          placeholder="请输入11位手机号码"
-          :error-message="error.mobile"
-        />
-        <van-field
-          v-model="form.password"
-          type="password"
-          label="密码"
-          required
-          clearable
-          placeholder="请输入 6~18 位的数字、字母、下划线做为密码"
-          left-icon="lock"
-          :error-message="error.password"
-        />
-      </van-cell-group>
-      <van-button @click="login" class="login-btn" type="danger">登录</van-button>
-      <van-button @click="$router.push('/regist')" class="login-btn" type="warning">没有账号？点击注册</van-button>
+    <van-cell-group>
+      <van-field
+        v-model="form.mobile"
+        required
+        clearable
+        left-icon="manager"
+        label="手机号码"
+        placeholder="请输入11位手机号码"
+        :error-message="error.mobile"
+      />
+      <van-field
+        v-model="form.password"
+        type="password"
+        label="密码"
+        required
+        clearable
+        placeholder="请输入 6~18 位的数字、字母、下划线做为密码"
+        left-icon="lock"
+        :error-message="error.password"
+      />
+    </van-cell-group>
+    <van-button @click="login" class="login-btn" type="danger">登录</van-button>
+    <van-button @click="$router.push('/regist')" class="login-btn" type="warning">没有账号？点击注册</van-button>
   </div>
 </template>
 
@@ -47,13 +44,17 @@ export default {
   },
   methods: {
     login () {
+      // 正则验证手机号
       let mobileRe = /^1(3|5|7|8|9)\d{9}$/
+      // 判断
       if (!mobileRe.test(this.form.mobile)) {
         this.error.mobile = '手机号码格式不正确'
         return false
       }
 
+      // 正则验证密码
       let passwordRe = /^[0-9a-zA-Z_]{6,18}$/
+      // 判断
       if (!passwordRe.test(this.form.password)) {
         this.error.password = '密码必须是6~18位数字、字母、下划线'
         return false
@@ -61,7 +62,9 @@ export default {
 
       // 创建一个 URLSearchParams 对象
       let formData = new URLSearchParams()
+      // 手机号码
       formData.append('mobile', this.form.mobile)
+      // 密码
       formData.append('password', this.form.password)
 
       this.$http.post('/login', formData).then(res => {
@@ -80,14 +83,14 @@ export default {
 </script>
 
 <style>
-  .login .van-cell-group {
-    margin-top: 100px;
-  }
-  .login {
-    text-align: center;
-  }
-  .login .login-btn {
-    margin-top: 50px;
-    width: 70%;
-  }
+.login .van-cell-group {
+  margin-top: 100px;
+}
+.login {
+  text-align: center;
+}
+.login .login-btn {
+  margin-top: 50px;
+  width: 70%;
+}
 </style>
